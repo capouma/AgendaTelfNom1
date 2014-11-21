b997e9431bb45d257eeb0db35600bf9a187d16b5
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-11-21 13:05:24 +0100

    Creamos una clase JAVA llamada Contactos con sus constructores

---
+++ app/src/main/java/com/example/findag/agendatelfnom/Contactos.java
@@ -0,0 +1,26 @@
+package com.example.findag.agendatelfnom;
+
+/**
+ * Created by findag on 21/11/14.
+ */
+public class Contactos
+{
+    // Definimos las variables que tendremos.
+    String nombre;
+    String telefono;
+
+    // Creamos un constructor vacio.
+    public Contactos()
+    {
+
+    }
+
+    // Creamos un constructor con parametros.
+    public Contactos(String nombre, String telefono)
+    {
+        this.nombre = nombre;
+        this.telefono = telefono;
+
+    }
+
+}
