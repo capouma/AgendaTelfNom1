a560d5698a4781659e033089795fc055f559795c
cpousamarcote <cpousamarcote@danielcastelao.org>
2014-11-22 16:29:01 +0100

    Recogemos el objeto intent de nuestra Activity1 y modificamos  el nombre o el apellido o ambos y reenviamos los cambios.

--- app/src/main/java/com/example/findag/agendatelfnom/Activity2.java
+++ app/src/main/java/com/example/findag/agendatelfnom/Activity2.java
@@ -1,17 +1,52 @@
 package com.example.findag.agendatelfnom;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 
-public class Activity2 extends Activity {
+public class Activity2 extends Activity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_activity2);
+
+        // Definimos el EditText nombre y telefono y el boton Cambiar.
+        final EditText edtNombre = (EditText) findViewById(R.id.edtNombre2);
+        final EditText edtTelefono = (EditText) findViewById(R.id.edtTelefono2);
+        Button btnCambiar = (Button) findViewById(R.id.btnCambiar);
+
+        //Creamos un objeto de tipo Contactos para recoger el objeto que enviamos en nuestro intent
+        Contactos aEditar = (Contactos) getIntent().getSerializableExtra("editando");
+
+        // Insertamos los datos recogidos en nuestro objeto aEditar en nuestraos EditText nombre y telefono.
+        edtNombre.setText(aEditar.nombre);
+        edtTelefono.setText(aEditar.telefono);
+
+        //Creamos el evento onClick de nuestro boton Cambiar.
+        btnCambiar.setOnClickListener(new View.OnClickListener()
+        {
+            //Sobreescribimos el metodo onClick para que recoja los cambios realizados en el contacto y los envie a la Activity1.
+            @Override
+            public void onClick(View v)
+            {
+                Intent modificado = new Intent();
+                Contactos editado = new Contactos(edtNombre.getText().toString(), edtTelefono.getText().toString());
+                modificado.putExtra("contactoEditado",editado);
+                setResult(RESULT_OK, modificado);
+                finish();
+            }
+        });
+
+
     }
 
 
